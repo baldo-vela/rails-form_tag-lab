77d747752c590de4d8f4adda64ec59c211947380
baldo-vela <baldo.vela+github@gmail.com>
2021-01-18 14:33:32 -0600

    Form is done?

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,9 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to new_students_path
+  end
+
 end
